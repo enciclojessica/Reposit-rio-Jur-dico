7166c0a77b461800432731e4451cf530acfa63b5
enciclojessica <foxjessica01@gmail.com>
2026-05-25 11:45:36 -0300

    arquivos iniciais

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://qfoxgipvhudpbsyijtaz.supabase.co',
-  'sb_publishable_jhdi8QvAr_V3bU17TFibCA_Jsa-TAyZ'
-)
