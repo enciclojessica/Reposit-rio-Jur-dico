101034d53c87d7013c16f368cfaef9a3612f2900
enciclojessica <foxjessica01@gmail.com>
2026-06-07 21:16:34 -0300

    fix: substituir sb_publishable por anon JWT key

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://qfoxgipvhudpbsyijtaz.supabase.co',
-  'sb_publishable_jhdi8QvAr_V3bU17TFibCA_Jsa-TAyZ'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmb3hnaXB2aHVkcGJzeWlqdGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDk5NTQsImV4cCI6MjA5NTI4NTk1NH0.OqtJdybi5lgwoU6jPL48QXb1BSl2Tq8rySvHw_qnHy8'
 )
+
